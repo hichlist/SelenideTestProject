3214d0c68e508c86a15cca095a0b001997486873
Igor Pischurkov <ip@glonassmobile.com>
2024-10-11 16:55:45 +0400

    tests auth, booksInfo, profile

--- src/test/java/TestDemoQa.java
+++ src/test/java/TestDemoQa.java
@@ -2,25 +2,53 @@ import com.codeborne.selenide.Configuration;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Configuration.*;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class TestDemoQa {
+    private final String userName = "hich";
+    private final String password = "!QAZ1qaz";
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 
     @BeforeClass
     public static void setup() {
-        Configuration.browser = "chrome";
+        Configuration.browser = "firefox";
         baseUrl = "https://demoqa.com/";
-        timeout = 40000;
         open(baseUrl+"books");
-
     }
 
     @Test
     public void testAuth() {
         $("#login").click();
+        $("#userName").setValue(getUserName());
+        $("#password").setValue(getPassword());
+        $("#login").click();
+        $("#userName-value").shouldHave(text(getUserName()));
     }
+
+    @Test
+    public void testBooksInfo() {
+        $$(".rt-tr-group").shouldHave(size(10));
+        $(".mr-2 a").shouldHave(text("Git Pocket Guide"));
+        $(".rt-tr-group").shouldHave(text("Richard E. Silverman"));
+        $(".rt-tr-group").shouldHave(text("O'Reilly Media"));
+    }
+
+    @Test
+    public void testProfile() {
+        open(baseUrl+"profile");
+//        $("#item-3").shouldBe(visible, clickable).click();
+        $("#userName-value").shouldHave(text(getUserName()));
+    }
+
 
 }
